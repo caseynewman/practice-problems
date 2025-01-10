b894f90345748d6aa041be1fc2df36b76231ff23
Yogi Spice <caseynnewman@yahoo.com>
2025-01-10 14:20:46 -0500

    added front22 problem and solution

---
+++ warmup-1/front22/index.js
@@ -0,0 +1,6 @@
+// Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so 'kitten' yields 'kikittenki' chars are there.
+
+
+function front22(str) {
+    return str.substring(0, 2) + str + str.substring(0, 2)
+}
